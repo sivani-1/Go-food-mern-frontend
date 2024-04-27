27cc7485472921f42fb769eb3fa62ba665795316
sivani-1 <147125105+sivani-1@users.noreply.github.com>
2024-04-27 10:31:01 +0530

    change made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css';
+import { CartProvider } from './components/ContextReducer.js';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import MyOrder from './screens/MyOrder.js';
+import Signup from './screens/Signup.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+        <BrowserRouter>
+            <Routes> 
+              <Route exact path="/" element={<Home/> } />
+              <Route exact path="/login" element={<Login/> } />
+              <Route exact path="/createuser" element={<Signup/> } />
+              <Route exact path="/myorder" element={<MyOrder/> } />
+            </Routes>
+        </BrowserRouter>
+    </CartProvider>
   );
 }
 
---
+++ src/screens/Home.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from 'react';
+import Card from '../components/Card';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
+export default function Home() {
+  const [search, setSearch] = useState('');
+  const [foodCat,setFoodCat] = useState([]);
+  const [foodItem,setFoodItem] = useState([]);
+
+  const loadData = async () =>{
+    let response = await fetch("http://localhost:5000/api/foodData",{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    response = await response.json();
+
+    setFoodItem(response[0]);
+    setFoodCat(response[1]);
+    // console.log(response[0],response[1]);
+
+  }
+
+  useEffect(() => {
+    loadData()
+  },[])
+
+  
+  return (
+    <div>
+        <div> <Navbar/> </div>
+        <div> 
+          {/* <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel" style={{objectFit:"contain !important",with:" 5px"}}>
+          <div className="carousel-inner" style={{zIndex:"10"}}> */}
+              <div className="d-flex justify-content-center mt-5 ">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e)=>{setSearch(e.target.value)}} style={{width:"300px"}}/>
+                <button className="btn btn-outline-success text-white bg-success" type="submit">Search</button>
+              </div>
+              {/* <div className="carousel-item active">
+              <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" alt="..."/>
+              </div>
+              <div className="carousel-item">
+              <img src="https://source.unsplash.com/random/900x700/?momos" className="d-block w-100" alt="..."/>
+              </div>
+              <div className="carousel-item">
+              <img src="https://source.unsplash.com/random/900x700/?sea" className="d-block w-100" alt="..."/>
+              </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+          </button>
+          </div> */}
+        </div>
+        <div className='container'>
+          {
+            foodCat !=[]
+            ? foodCat.map((data)=>{
+              return ( <div className='row mb-3'>
+              <div key={data._id} className="fs-3 m-3">
+                {data.CategoryName}
+              </div>
+              <hr />
+              {foodItem != []
+                ? 
+                foodItem.filter((item) => (item.CategoryName === data.CategoryName) && (item.name.toLowerCase().includes(search.toLowerCase())))
+                .map(filterItems=>{
+                  return(
+                    <div key={filterItems._id} className='col-12 col-md-6 col-lg-3'>
+                      <Card foodItem = {filterItems}
+                      //foodName = {filterItems.name}
+                      options={filterItems.options[0]}
+                      //imgSrc = {filterItems.img}
+                      ></Card>
+                    </div>
+                  )
+                })
+                : <div> No such data</div>
+              }
+              </div>
+              )
+            }): ""
+          }
+        </div>
+        <div> <Footer/> </div>
+    </div>
+  )
+}
